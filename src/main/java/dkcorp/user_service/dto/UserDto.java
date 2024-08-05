66d05cfcda2d0e29ab1447547887969d28b69647
Denis Karpov <karpovdenis.al@gmail.com>
2024-08-05 16:57:59 +0300

    Create user dto

---
+++ src/main/java/dkcorp/user_service/dto/UserDto.java
@@ -0,0 +1,21 @@
+package dkcorp.user_service.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+    private Long id;
+    private String username;
+    private String email;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private Long countryId;
+}
