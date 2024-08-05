6d9e60f2fffe5220ca6797895a8a7844f34bf80a
Denis Karpov <karpovdenis.al@gmail.com>
2024-08-05 17:00:26 +0300

    Create user mapper

---
+++ src/main/java/dkcorp/user_service/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package dkcorp.user_service.mapper;
+
+import dkcorp.user_service.dto.UserDto;
+import dkcorp.user_service.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+import java.util.List;
+
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = "spring")
+public interface UserMapper {
+    UserDto toDto(User user);
+
+    User toEntity(UserDto userDto);
+
+    List<UserDto> toDtoList(List<User> users);
+}
