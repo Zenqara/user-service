8e278c208c2464d1f4399a0de7abd0613b4cdbf6
Denis Karpov <karpovdenis.al@gmail.com>
2024-08-09 14:06:24 +0300

    Add new user fields

--- src/main/java/dkcorp/user_service/dto/UserDto.java
+++ src/main/java/dkcorp/user_service/dto/UserDto.java
@@ -2,20 +2,26 @@ package dkcorp.user_service.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserDto {
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Long id;
     private String username;
     private String email;
+    private String phone;
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String firstName;
+    private String lastName;
+    private String aboutMe;
+    private LocalDateTime dateOfBirth;
+    private String city;
     private Long countryId;
 }
