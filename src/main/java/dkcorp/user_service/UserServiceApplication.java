ab1e739ecb4267357ecaaa06152ac36e7d1bc199
Denis Karpov <karpovdenis.al@gmail.com>
2024-08-09 14:04:22 +0300

    Add ObjectMapper bean

--- src/main/java/dkcorp/user_service/UserServiceApplication.java
+++ src/main/java/dkcorp/user_service/UserServiceApplication.java
@@ -1,13 +1,22 @@
 package dkcorp.user_service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class UserServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
 
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        return objectMapper;
+    }
 }
