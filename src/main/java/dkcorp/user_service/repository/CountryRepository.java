aa082bd1297c1419b3a9505749a246eb9b027f1f
Denis Karpov <karpovdenis.al@gmail.com>
2024-08-11 16:27:02 +0300

    Remove country entity

--- src/main/java/dkcorp/user_service/repository/CountryRepository.java
+++
@@ -1,7 +0,0 @@
-package dkcorp.user_service.repository;
-
-import dkcorp.user_service.entity.Country;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CountryRepository extends JpaRepository<Country, Long> {
-}
