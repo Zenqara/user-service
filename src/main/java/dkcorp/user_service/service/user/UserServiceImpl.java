8a3f2ca1d7617c755e18318ea44ae1002dd4f836
Denis Karpov <karpovdenis.al@gmail.com>
2024-08-11 12:47:18 +0300

    Reorder methods, refactor userId type, change update method

--- src/main/java/dkcorp/user_service/service/user/UserServiceImpl.java
+++ src/main/java/dkcorp/user_service/service/user/UserServiceImpl.java
@@ -1,50 +1,20 @@
 package dkcorp.user_service.service.user;
 
 import dkcorp.user_service.dto.UserDto;
-import dkcorp.user_service.entity.Country;
 import dkcorp.user_service.entity.User;
 import dkcorp.user_service.exception.EntityNotFoundException;
 import dkcorp.user_service.mapper.UserMapper;
 import dkcorp.user_service.repository.UserRepository;
-import dkcorp.user_service.service.country.CountryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
-    private final CountryService countryService;
     private final UserMapper userMapper;
-
-    @Override
-    public UserDto createUser(UserDto userDto) {
-        Country country = countryService.findById(userDto.getCountryId());
-        User newUser = userMapper.toEntity(userDto);
-        newUser.setCountry(country);
-        newUser.setActive(true);
-        return userMapper.toDto(newUser);
-    }
-
-    @Override
-    public UserDto findById(long userId) {
-        return userMapper.toDto(findUserById(userId));
-    }
-
-    @Override
-    public boolean existsById(long userId) {
-        return userRepository.existsById(userId);
-    }
-
-    @Override
-    public UserDto deactivateUser(long userId) {
-        User user = findUserById(userId);
-        user.setActive(false);
-        return userMapper.toDto(userRepository.save(user));
-    }
 
     @Override
     public List<UserDto> findAll() {
@@ -53,14 +23,32 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public UserDto update(UserDto userDto) {
-        User updatedUser = userMapper.toEntity(userDto);
-        updatedUser.setUpdatedAt(LocalDateTime.now());
-        userRepository.save(updatedUser);
-        return userMapper.toDto(updatedUser);
+    public UserDto findById(Long userId) {
+        return userMapper.toDto(findUserById(userId));
     }
 
-    private User findUserById(long userId) {
+    @Override
+    public UserDto createUser(UserDto userDto) {
+        User newUser = userMapper.toEntity(userDto);
+        newUser.setActive(true);
+        return userMapper.toDto(userRepository.save(newUser));
+    }
+
+    @Override
+    public UserDto updateUser(Long userId, UserDto userDto) {
+        User existingUser = findUserById(userId);
+        userMapper.updateUserFromDto(userDto, existingUser);
+        return userMapper.toDto(userRepository.save(existingUser));
+    }
+
+    @Override
+    public UserDto deactivateUser(Long userId) {
+        User user = findUserById(userId);
+        user.setActive(false);
+        return userMapper.toDto(userRepository.save(user));
+    }
+
+    private User findUserById(Long userId) {
         return userRepository.findById(userId).orElseThrow(() -> new EntityNotFoundException(String.format("User with id=%d not found", userId)));
     }
 }
