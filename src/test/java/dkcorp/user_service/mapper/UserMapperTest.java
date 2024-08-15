e25a1f4134279f65a3b785d9d9a0fe385796255f
Denis Karpov <karpovdenis.al@gmail.com>
2024-08-15 16:42:17 +0300

    Remove redundant checks

--- src/test/java/dkcorp/user_service/mapper/UserMapperTest.java
+++ src/test/java/dkcorp/user_service/mapper/UserMapperTest.java
@@ -7,52 +7,78 @@ import org.junit.jupiter.api.Test;
 import org.mapstruct.factory.Mappers;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class UserMapperTest {
     private final UserMapper userMapper = Mappers.getMapper(UserMapper.class);
 
     @Test
-    void mapEntityToDtoTest() {
+    void entityToDto_shouldConvertUserToUserDto() {
         User user = User.builder()
                 .id(1L)
-                .username("IvanKiller2014")
-                .email("ivanivanov@gmail.com")
-                .phone("123456789440")
-                .password("securepassword123")
-                .firstName("Ivan")
-                .lastName("Doe")
-                .aboutMe("I like pizza")
-                .dateOfBirth(LocalDate.parse("2014-01-15"))
-                .active(true)
-                .createdAt(LocalDateTime.now())
-                .updatedAt(LocalDateTime.now())
-                .subscribers(new HashSet<>())
-                .subscribedUsers(new HashSet<>())
+                .username("john_doe")
+                .email("john@example.com")
                 .build();
 
         UserDto userDto = userMapper.entityToDto(user);
 
-        assertNotNull(userDto);
         assertEquals(user.getId(), userDto.getId());
         assertEquals(user.getUsername(), userDto.getUsername());
         assertEquals(user.getEmail(), userDto.getEmail());
-        assertEquals(user.getPhone(), userDto.getPhone());
-        assertEquals(user.getFirstName(), userDto.getFirstName());
-        assertEquals(user.getLastName(), userDto.getLastName());
-        assertEquals(user.getAboutMe(), userDto.getAboutMe());
-        assertEquals(user.getDateOfBirth(), userDto.getDateOfBirth());
-        assertTrue(userDto.isActive());
-        assertEquals(user.getCreatedAt(), userDto.getCreatedAt());
-        assertEquals(user.getUpdatedAt(), userDto.getUpdatedAt());
+    }
+
+    @Test
+    void modifyDtoToEntity_shouldConvertUserModifyDtoToUser() {
+        UserModifyDto userModifyDto = UserModifyDto.builder()
+                .username("john_doe")
+                .email("john@example.com")
+                .password("Pa$$w0rd")
+                .dateOfBirth(LocalDate.of(1990, 1, 1))
+                .build();
+
+        User user = userMapper.modifyDtoToEntity(userModifyDto);
+
+        assertEquals(userModifyDto.getUsername(), user.getUsername());
+        assertEquals(userModifyDto.getEmail(), user.getEmail());
+    }
+
+    @Test
+    void toDtoList_shouldConvertListOfUsersToListOfUserDtos() {
+        List<User> users = List.of(
+                User.builder().id(1L).username("john_doe").email("john@example.com").build(),
+                User.builder().id(2L).username("jane_doe").email("jane@example.com").build()
+        );
+
+        List<UserDto> userDtos = userMapper.toDtoList(users);
+
+        assertEquals(users.size(), userDtos.size());
+        for (int i = 0; i < users.size(); i++) {
+            assertEquals(users.get(i).getId(), userDtos.get(i).getId());
+            assertEquals(users.get(i).getUsername(), userDtos.get(i).getUsername());
+            assertEquals(users.get(i).getEmail(), userDtos.get(i).getEmail());
+        }
+    }
+
+    @Test
+    void updateUserFromModifyDto_shouldUpdateExistingUser() {
+        User existingUser = User.builder()
+                .id(1L)
+                .username("john_doe")
+                .email("john@example.com")
+                .build();
+
+        UserModifyDto userModifyDto = UserModifyDto.builder()
+                .username("john_doe_updated")
+                .email("john_updated@example.com")
+                .build();
+
+        userMapper.updateUserFromModifyDto(userModifyDto, existingUser);
+
+        assertEquals(userModifyDto.getUsername(), existingUser.getUsername());
+        assertEquals(userModifyDto.getEmail(), existingUser.getEmail());
     }
 
     @Test
@@ -62,213 +88,15 @@ public class UserMapperTest {
     }
 
     @Test
-    void mapModifyDtoToEntityTest() {
-        UserModifyDto userModifyDto = UserModifyDto.builder()
-                .username("IvanKiller2014")
-                .email("ivanivanov@gmail.com")
-                .phone("123456789440")
-                .password("securepassword123")
-                .firstName("Ivan")
-                .lastName("Doe")
-                .aboutMe("I like pizza")
-                .dateOfBirth(LocalDate.parse("2014-01-15"))
-                .build();
-
-        User user = userMapper.modifyDtoToEntity(userModifyDto);
-
-        assertNotNull(user);
-        assertEquals(userModifyDto.getUsername(), user.getUsername());
-        assertEquals(userModifyDto.getEmail(), user.getEmail());
-        assertEquals(userModifyDto.getPhone(), user.getPhone());
-        assertEquals(userModifyDto.getFirstName(), user.getFirstName());
-        assertEquals(userModifyDto.getLastName(), user.getLastName());
-        assertEquals(userModifyDto.getAboutMe(), user.getAboutMe());
-        assertEquals(userModifyDto.getDateOfBirth(), user.getDateOfBirth());
-    }
-
-    @Test
     void mapNullModifyDtoToUserTest() {
         User user = userMapper.modifyDtoToEntity(null);
         assertNull(user);
     }
 
-    @Test
-    void mapUserListToDtoListTest() {
-        User user1 = User.builder()
-                .id(1L)
-                .username("IvanKiller2014")
-                .email("ivanivanov@gmail.com")
-                .phone("1234567890")
-                .password("securepassword123")
-                .firstName("Ivan")
-                .lastName("Doe")
-                .aboutMe("I like pizza")
-                .dateOfBirth(LocalDate.parse("2014-01-15"))
-                .active(true)
-                .createdAt(LocalDateTime.now())
-                .updatedAt(LocalDateTime.now())
-                .subscribers(new HashSet<>())
-                .subscribedUsers(new HashSet<>())
-                .build();
-
-        User user2 = User.builder()
-                .id(2L)
-                .username("JaneDoe2022")
-                .email("janedoe@gmail.com")
-                .phone("0987654321")
-                .password("anotherpassword321")
-                .firstName("Jane")
-                .lastName("Doe")
-                .aboutMe("I love hiking")
-                .dateOfBirth(LocalDate.parse("1990-05-22"))
-                .active(true)
-                .createdAt(LocalDateTime.now())
-                .updatedAt(LocalDateTime.now())
-                .subscribers(new HashSet<>())
-                .subscribedUsers(new HashSet<>())
-                .build();
-
-        User user3 = User.builder()
-                .id(3L)
-                .username("JohnSmith")
-                .email("johnsmith@gmail.com")
-                .phone("1122334455")
-                .password("mypassword123")
-                .firstName("John")
-                .lastName("Smith")
-                .aboutMe("I enjoy programming")
-                .dateOfBirth(LocalDate.parse("1985-10-10"))
-                .active(false)
-                .createdAt(LocalDateTime.now())
-                .updatedAt(LocalDateTime.now())
-                .subscribers(new HashSet<>())
-                .subscribedUsers(new HashSet<>())
-                .build();
-
-        List<User> users = new ArrayList<>();
-        users.add(user1);
-        users.add(user2);
-        users.add(user3);
-
-        List<UserDto> userDtoList = userMapper.toDtoList(users);
-
-        assertNotNull(userDtoList);
-        assertEquals(3, userDtoList.size());
-
-        for (int i = 0; i < users.size(); i++) {
-            User user = users.get(i);
-            UserDto userDto = userDtoList.get(i);
-
-            assertEquals(user.getId(), userDto.getId());
-            assertEquals(user.getUsername(), userDto.getUsername());
-            assertEquals(user.getEmail(), userDto.getEmail());
-            assertEquals(user.getPhone(), userDto.getPhone());
-            assertEquals(user.getFirstName(), userDto.getFirstName());
-            assertEquals(user.getLastName(), userDto.getLastName());
-            assertEquals(user.getAboutMe(), userDto.getAboutMe());
-            assertEquals(user.getDateOfBirth(), userDto.getDateOfBirth());
-            assertEquals(user.isActive(), userDto.isActive());
-            assertEquals(user.getCreatedAt(), userDto.getCreatedAt());
-            assertEquals(user.getUpdatedAt(), userDto.getUpdatedAt());
-        }
-    }
 
     @Test
     void mapNullUserListToDtoListTest() {
         List<UserDto> userDtoList = userMapper.toDtoList(null);
         assertNull(userDtoList);
-    }
-
-    @Test
-    void updateUserFromModifyDtoTest() {
-        User user = User.builder()
-                .id(1L)
-                .username("TestUser")
-                .email("testuser@gmail.com")
-                .phone("1234567890")
-                .password("password")
-                .firstName("Test")
-                .lastName("User")
-                .aboutMe("About me")
-                .dateOfBirth(LocalDate.now())
-                .active(true)
-                .createdAt(LocalDateTime.now())
-                .updatedAt(LocalDateTime.now())
-                .build();
-
-        String newUsername = "NewUsername";
-        String newEmail = "newemail@gmail.com";
-        String newPhone = "0987654321";
-        String newPassword = "newpassword";
-        String newFirstName = "NewFirstName";
-        String newLastName = "NewLastName";
-        String newAboutMe = "New about me";
-        LocalDate newDateOfBirth = LocalDate.parse("2000-01-01");
-
-        UserModifyDto userModifyDto = UserModifyDto.builder()
-                .username(newUsername)
-                .email(newEmail)
-                .phone(newPhone)
-                .password(newPassword)
-                .firstName(newFirstName)
-                .lastName(newLastName)
-                .aboutMe(newAboutMe)
-                .dateOfBirth(newDateOfBirth)
-                .build();
-
-        userMapper.updateUserFromModifyDto(userModifyDto, user);
-
-        assertEquals(newUsername, user.getUsername());
-        assertEquals(newEmail, user.getEmail());
-        assertEquals(newPhone, user.getPhone());
-        assertEquals(newPassword, user.getPassword());
-        assertEquals(newFirstName, user.getFirstName());
-        assertEquals(newLastName, user.getLastName());
-        assertEquals(newAboutMe, user.getAboutMe());
-        assertEquals(newDateOfBirth, user.getDateOfBirth());
-    }
-
-    @Test
-    void updateUserFromNullModifyDtoTest() {
-        User user = User.builder()
-                .id(1L)
-                .username("TestUser")
-                .email("testuser@gmail.com")
-                .phone("1234567890")
-                .password("password")
-                .firstName("Test")
-                .lastName("User")
-                .aboutMe("About me")
-                .dateOfBirth(LocalDate.now())
-                .active(true)
-                .createdAt(LocalDateTime.now())
-                .updatedAt(LocalDateTime.now())
-                .build();
-
-        String initialUsername = user.getUsername();
-        String initialEmail = user.getEmail();
-        String initialPhone = user.getPhone();
-        String initialPassword = user.getPassword();
-        String initialFirstName = user.getFirstName();
-        String initialLastName = user.getLastName();
-        String initialAboutMe = user.getAboutMe();
-        LocalDate initialDateOfBirth = user.getDateOfBirth();
-        boolean initialActive = user.isActive();
-        LocalDateTime initialCreatedAt = user.getCreatedAt();
-        LocalDateTime initialUpdatedAt = user.getUpdatedAt();
-
-        userMapper.updateUserFromModifyDto(null, user);
-
-        assertEquals(initialUsername, user.getUsername());
-        assertEquals(initialEmail, user.getEmail());
-        assertEquals(initialPhone, user.getPhone());
-        assertEquals(initialPassword, user.getPassword());
-        assertEquals(initialFirstName, user.getFirstName());
-        assertEquals(initialLastName, user.getLastName());
-        assertEquals(initialAboutMe, user.getAboutMe());
-        assertEquals(initialDateOfBirth, user.getDateOfBirth());
-        assertEquals(initialActive, user.isActive());
-        assertEquals(initialCreatedAt, user.getCreatedAt());
-        assertEquals(initialUpdatedAt, user.getUpdatedAt());
     }
 }
