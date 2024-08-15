9932c563a938f75bfed38a77d23092b8aa24d738
Denis Karpov <karpovdenis.al@gmail.com>
2024-08-15 16:45:06 +0300

    Create global exception handler

---
+++ src/main/java/dkcorp/user_service/exception/handle/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package dkcorp.user_service.exception.handle;
+
+import dkcorp.user_service.exception.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<String> handleEntityNotFoundException(EntityNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+}
