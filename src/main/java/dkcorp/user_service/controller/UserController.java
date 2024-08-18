11fa1117d8fcd72e7c8ff31b007254497bec29f9
Denis Karpov <karpovdenis.al@gmail.com>
2024-08-18 18:09:06 +0300

    Add validated annotation

--- src/main/java/dkcorp/user_service/controller/UserController.java
+++ src/main/java/dkcorp/user_service/controller/UserController.java
@@ -9,6 +9,7 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Validated
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/users")
