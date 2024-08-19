49d8a98edeffd1ec69ee95979e36adb25d26e25a
Denis Karpov <karpovdenis.al@gmail.com>
2024-08-19 15:46:53 +0300

    Create api error dto

---
+++ src/main/java/dkcorp/user_service/dto/ApiErrorDto.java
@@ -0,0 +1,20 @@
+package dkcorp.user_service.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiErrorDto {
+    private String message;
+    private String field;
+    private String path;
+    private String status;
+    private long timestamp;
+}
