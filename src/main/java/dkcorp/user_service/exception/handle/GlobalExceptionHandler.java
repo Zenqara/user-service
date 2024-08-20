d353fd3ffa50ebf041d3c8f00316901cc5cd89db
Denis Karpov <karpovdenis.al@gmail.com>
2024-08-20 16:35:30 +0300

    Add handler for validation exceptions

--- src/main/java/dkcorp/user_service/exception/handle/GlobalExceptionHandler.java
+++ src/main/java/dkcorp/user_service/exception/handle/GlobalExceptionHandler.java
@@ -5,6 +5,8 @@ import dkcorp.user_service.exception.EntityNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -19,5 +21,21 @@ public class GlobalExceptionHandler {
                 .timestamp(System.currentTimeMillis())
                 .build();
         return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiErrorDto> handleValidationExceptions(MethodArgumentNotValidException ex, HttpServletRequest request) {
+        FieldError fieldError = ex.getBindingResult().getFieldError();
+        if (fieldError != null) {
+            ApiErrorDto apiError = ApiErrorDto.builder()
+                    .message(fieldError.getDefaultMessage())
+                    .field(fieldError.getField())
+                    .path(request.getRequestURI())
+                    .status(HttpStatus.BAD_REQUEST.toString())
+                    .timestamp(System.currentTimeMillis())
+                    .build();
+            return new ResponseEntity<>(apiError, HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 }
