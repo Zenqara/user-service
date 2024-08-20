9b014016a324661ec52a9cbec4668bb400c276c7
Denis Karpov <karpovdenis.al@gmail.com>
2024-08-20 16:33:18 +0300

    Refactor to use api error dto

--- src/main/java/dkcorp/user_service/exception/handle/GlobalExceptionHandler.java
+++ src/main/java/dkcorp/user_service/exception/handle/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package dkcorp.user_service.exception.handle;
 
+import dkcorp.user_service.dto.ApiErrorDto;
 import dkcorp.user_service.exception.EntityNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,7 +11,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<String> handleEntityNotFoundException(EntityNotFoundException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    public ResponseEntity<ApiErrorDto> handleEntityNotFoundException(EntityNotFoundException ex, HttpServletRequest request) {
+        ApiErrorDto apiError = ApiErrorDto.builder()
+                .message(ex.getMessage())
+                .path(request.getRequestURI())
+                .status(HttpStatus.NOT_FOUND.toString())
+                .timestamp(System.currentTimeMillis())
+                .build();
+        return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
     }
 }
