de0980f105cfbaf5a93c73e6fb065d9a3cce9f7c
Denis Karpov <karpovdenis.al@gmail.com>
2024-08-23 12:00:54 +0300

    Create test method for validation exceptions

--- src/test/java/dkcorp/user_service/exception/handle/GlobalExceptionHandlerTest.java
+++ src/test/java/dkcorp/user_service/exception/handle/GlobalExceptionHandlerTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import java.util.Objects;
 
@@ -31,5 +33,30 @@ class GlobalExceptionHandlerTest {
         assertEquals(errorMessage, Objects.requireNonNull(response.getBody()).getMessage());
         assertEquals(requestUri, response.getBody().getPath());
         assertEquals(HttpStatus.NOT_FOUND.toString(), response.getBody().getStatus());
+    }
+
+    @Test
+    void testHandleValidationExceptions() {
+        String fieldName = "username";
+        String errorMessage = "Username cannot be empty";
+        String requestUri = "/api/v1/users";
+
+        FieldError fieldError = Mockito.mock(FieldError.class);
+        when(fieldError.getDefaultMessage()).thenReturn(errorMessage);
+        when(fieldError.getField()).thenReturn(fieldName);
+
+        MethodArgumentNotValidException ex = Mockito.mock(MethodArgumentNotValidException.class);
+        when(ex.getBindingResult().getFieldError()).thenReturn(fieldError);
+
+        HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
+        when(request.getRequestURI()).thenReturn(requestUri);
+
+        ResponseEntity<ApiErrorDto> response = globalExceptionHandler.handleValidationExceptions(ex, request);
+
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals(errorMessage, Objects.requireNonNull(response.getBody()).getMessage());
+        assertEquals(fieldName, response.getBody().getField());
+        assertEquals(requestUri, response.getBody().getPath());
+        assertEquals(HttpStatus.BAD_REQUEST.toString(), response.getBody().getStatus());
     }
 }
