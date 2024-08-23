04c00541b522fe595c6b0bca8403dda6df9d153f
Denis Karpov <karpovdenis.al@gmail.com>
2024-08-23 11:59:37 +0300

    Use api error dto in handle entity not found test

--- src/test/java/dkcorp/user_service/exception/handle/GlobalExceptionHandlerTest.java
+++ src/test/java/dkcorp/user_service/exception/handle/GlobalExceptionHandlerTest.java
@@ -1,11 +1,17 @@
 package dkcorp.user_service.exception.handle;
 
+import dkcorp.user_service.dto.ApiErrorDto;
 import dkcorp.user_service.exception.EntityNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.util.Objects;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 class GlobalExceptionHandlerTest {
     private final GlobalExceptionHandler globalExceptionHandler = new GlobalExceptionHandler();
@@ -13,11 +19,17 @@ class GlobalExceptionHandlerTest {
     @Test
     void testHandleEntityNotFoundException() {
         String errorMessage = "Entity not found";
-        EntityNotFoundException exception = new EntityNotFoundException(errorMessage);
+        String requestUri = "/api/v1/users/1";
+        EntityNotFoundException ex = new EntityNotFoundException(errorMessage);
 
-        ResponseEntity<String> response = globalExceptionHandler.handleEntityNotFoundException(exception);
+        HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
+        when(request.getRequestURI()).thenReturn(requestUri);
+
+        ResponseEntity<ApiErrorDto> response = globalExceptionHandler.handleEntityNotFoundException(ex, request);
 
         assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
-        assertEquals(errorMessage, response.getBody());
+        assertEquals(errorMessage, Objects.requireNonNull(response.getBody()).getMessage());
+        assertEquals(requestUri, response.getBody().getPath());
+        assertEquals(HttpStatus.NOT_FOUND.toString(), response.getBody().getStatus());
     }
 }
