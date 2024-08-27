7e0f0234387511e60142e34f10173852d07263f1
Denis Karpov <karpovdenis.al@gmail.com>
2024-08-27 17:09:00 +0300

    Add validate unsubscription method

--- src/main/java/dkcorp/user_service/validator/subscription/SubscriptionValidator.java
+++ src/main/java/dkcorp/user_service/validator/subscription/SubscriptionValidator.java
@@ -2,4 +2,6 @@ package dkcorp.user_service.validator.subscription;
 
 public interface SubscriptionValidator {
     void validateSubscription(Long followerId, Long followeeId);
+
+    void validateUnsubscription(Long followerId, Long followeeId);
 }
