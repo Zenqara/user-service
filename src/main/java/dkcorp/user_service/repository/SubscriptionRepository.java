f42aedc8dda73011e774de140f14d52483086548
Denis Karpov <karpovdenis.al@gmail.com>
2024-08-27 16:49:52 +0300

    Create is following method

--- src/main/java/dkcorp/user_service/repository/SubscriptionRepository.java
+++ src/main/java/dkcorp/user_service/repository/SubscriptionRepository.java
@@ -9,4 +9,7 @@ public interface SubscriptionRepository extends JpaRepository<User, Long> {
     @Modifying
     @Query(value = "INSERT INTO subscription (follower_user_id, followe_user_id) VALUES (:followerId, :followeeId)", nativeQuery = true)
     void subscribe(Long followerId, Long followeeId);
+
+    @Query(value = "SELECT CASE WHEN COUNT(*) > 0 THEN TRUE ELSE FALSE END FROM subscription WHERE follower_user_id = :followerId AND followee_user_id = :followeeId", nativeQuery = true)
+    boolean isFollowing(Long followerId, Long followeeId);
 }
