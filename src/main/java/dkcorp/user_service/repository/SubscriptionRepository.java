40824f3042fcbe7da1cd9dfd722652f484031d26
Denis Karpov <karpovdenis.al@gmail.com>
2024-08-27 16:35:20 +0300

    Create custom subscribe method

--- src/main/java/dkcorp/user_service/repository/SubscriptionRepository.java
+++ src/main/java/dkcorp/user_service/repository/SubscriptionRepository.java
@@ -2,6 +2,11 @@ package dkcorp.user_service.repository;
 
 import dkcorp.user_service.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface SubscriptionRepository extends JpaRepository<User, Long> {
+    @Modifying
+    @Query(value = "INSERT INTO subscription (follower_user_id, followe_user_id) VALUES (:followerId, :followeeId)", nativeQuery = true)
+    void subscribe(Long followerId, Long followeeId);
 }
