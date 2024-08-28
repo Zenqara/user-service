a9c38bf14dd6386920d17a5a472691423f3a8c96
Denis Karpov <karpovdenis.al@gmail.com>
2024-08-28 11:43:45 +0300

    Add method findFollowersByFolloweeId

--- src/main/java/dkcorp/user_service/repository/SubscriptionRepository.java
+++ src/main/java/dkcorp/user_service/repository/SubscriptionRepository.java
@@ -5,15 +5,22 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface SubscriptionRepository extends JpaRepository<User, Long> {
     @Modifying
     @Query(value = "INSERT INTO subscription (follower_user_id, followee_user_id) VALUES (:followerId, :followeeId)", nativeQuery = true)
-    void follow(Long followerId, Long followeeId);  // Было: subscribe
+    void follow(Long followerId, Long followeeId);
 
     @Modifying
     @Query(value = "DELETE FROM subscription WHERE follower_user_id = :followerId AND followee_user_id = :followeeId", nativeQuery = true)
-    void unfollow(Long followerId, Long followeeId);  // Было: unsubscribe
+    void unfollow(Long followerId, Long followeeId);
 
     @Query(value = "SELECT CASE WHEN COUNT(*) > 0 THEN TRUE ELSE FALSE END FROM subscription WHERE follower_user_id = :followerId AND followee_user_id = :followeeId", nativeQuery = true)
-    boolean isFollowing(Long followerId, Long followeeId);  // Было: isSubscribing
+    boolean isFollowing(Long followerId, Long followeeId);
+
+    @Query(value = "SELECT u.* FROM users u " +
+            "INNER JOIN subscription s ON u.id = s.follower_user_id " +
+            "WHERE s.followee_user_id = :followeeId", nativeQuery = true)
+    List<User> findFollowersByFolloweeId(Long followeeId);
 }
