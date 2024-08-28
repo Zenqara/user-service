b210bd2ea09a9391ce0de9c1a4a16ec5caf2c547
Denis Karpov <karpovdenis.al@gmail.com>
2024-08-28 10:58:43 +0300

    Refactor methods

--- src/main/java/dkcorp/user_service/validator/subscription/SubscriptionValidatorImpl.java
+++ src/main/java/dkcorp/user_service/validator/subscription/SubscriptionValidatorImpl.java
@@ -8,23 +8,46 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class SubscriptionValidatorImpl implements SubscriptionValidator {
+    private static final String ERROR_SELF_SUBSCRIPTION = "User cannot subscribe to himself";
+    private static final String ERROR_SELF_UNSUBSCRIPTION = "User cannot unsubscribe from himself";
+    private static final String ERROR_ALREADY_FOLLOWING = "User with id=%d is already following user with id=%d";
+    private static final String ERROR_NOT_FOLLOWING = "User with id=%d cannot unsubscribe from user with id=%d because he is not subscribed";
+
     private final SubscriptionRepository subscriptionRepository;
 
     @Override
     public void validateSubscription(Long followerId, Long followeeId) {
-        if (followerId.equals(followeeId)) {
-            throw new DataValidationException("User cannot subscribe to himself");
-        } else if (isFollowing(followerId, followeeId)) {
-            throw new DataValidationException(String.format("User with id=%d is already following user with id=%d", followerId, followeeId));
-        }
+        checkSelfSubscription(followerId, followeeId);
+        checkAlreadyFollowing(followerId, followeeId);
     }
 
     @Override
     public void validateUnsubscription(Long followerId, Long followeeId) {
+        checkSelfUnsubscription(followerId, followeeId);
+        checkNotFollowing(followerId, followeeId);
+    }
+
+    private void checkSelfSubscription(Long followerId, Long followeeId) {
         if (followerId.equals(followeeId)) {
-            throw new DataValidationException("User cannot unsubscribe from himself");
-        } else if (!isFollowing(followerId, followeeId)) {
-            throw new DataValidationException(String.format("User with id=%d cannot unsubscribe from user with id=%d because he is not subscribed", followerId, followeeId));
+            throw new DataValidationException(ERROR_SELF_SUBSCRIPTION);
+        }
+    }
+
+    private void checkSelfUnsubscription(Long followerId, Long followeeId) {
+        if (followerId.equals(followeeId)) {
+            throw new DataValidationException(ERROR_SELF_UNSUBSCRIPTION);
+        }
+    }
+
+    private void checkAlreadyFollowing(Long followerId, Long followeeId) {
+        if (isFollowing(followerId, followeeId)) {
+            throw new DataValidationException(String.format(ERROR_ALREADY_FOLLOWING, followerId, followeeId));
+        }
+    }
+
+    private void checkNotFollowing(Long followerId, Long followeeId) {
+        if (!isFollowing(followerId, followeeId)) {
+            throw new DataValidationException(String.format(ERROR_NOT_FOLLOWING, followerId, followeeId));
         }
     }
 
